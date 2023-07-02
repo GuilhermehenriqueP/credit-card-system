3d452fbd1a8def6f894300ef8f599aa1db68d9da
Guilherme Pessoa <gui.senai@live.com>
2023-07-02 17:07:21 -0300

    Criando microsserviço de clientes

---
+++ msclients/msclients/src/main/java/io/github/cursodsousa/msclients/aplication/ClienteService.java
@@ -0,0 +1,26 @@
+package io.github.cursodsousa.msclients.aplication;
+
+import io.github.cursodsousa.msclients.domain.Cliente;
+import io.github.cursodsousa.msclients.infra.repository.ClienteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ClienteService {
+
+    private final ClienteRepository repository;
+
+    @Transactional
+    public Cliente save(Cliente cliente){
+        return repository.save(cliente);
+    }
+
+    public Optional<Cliente> getByCPF(String cpf){
+        return repository.findByCpf(cpf);
+    }
+
+}
