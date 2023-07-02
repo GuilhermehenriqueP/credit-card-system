9f3b9386077b8aecf7f2afc8a1c6bd842c7f29e5
Guilherme Pessoa <gui.senai@live.com>
2023-07-02 14:04:49 -0300

    microsservice client instanciate

---
+++ eurekaserver/eurekaserver/src/test/java/id/github/cursodsousa/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package id.github.cursodsousa.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
